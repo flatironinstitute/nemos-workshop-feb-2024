acf752ff7170d3d3c3fc9dd41f8fdef32bfab805
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-01-10 12:40:12 -0500

    added loading code

--- docs/examples/glm_intro.py
+++ docs/examples/glm_intro.py
@@ -1,5 +1,12 @@
 r"""
-# What is a Generalized Linear Model (GLM)?
+
+# Generalized Linear Models (GLM): an Introduction
+
+This tutorial aim to familiarize the neurosceince audience with the concept of GLM. The user will learn about
+the fundamentals of the GLM theory, and apply the concepts to the analysis of an intracellularly recorded neuron.
+
+
+## What is a Generalized Linear Model (GLM)?
 
 !!! warning
     Some literature uses GLM to refer General Linear Models.
@@ -71,44 +78,118 @@ not strictly true in real data [REF].
     If the function $f$ is invertible, its inverse, denoted by $f^{-1}$, is commonly referred to as
     the **link function** in statistical literature.
 
-## Setting up a Poisson GLM
 
-*Work out minimal example of a poisson glm for predicting counts given an injected current.*
+## Setting up the Poisson GLM
 
-# Learning The GLM Weights by Maximum Likelihood (ML)
+### Dataset
+In this tutorial we will focus on a dataset from the Allen Cell Types data set.
+Our dataset consists of a whole cell current clamp recordings of an individual neurons.
+The neuron is stimulated with an injected current that follows a varieties of protocols: short and long square waves,
+ramps, withe noise, impulses.
 
-In general, we are provided with paired observations $(y_t, \bm{x}_t)$, for samples $t=1,...,T$,
-where $y_t$ are the spike counts and $\bm{x}_t$ are some simultaneously recorded input features. If we evaluate
-the encoding map (1) at the observations, we are left with a function of the weights, which is referred to as the
-model likelihood. The ML estimator for the model weights is the set of weights that maximizes the (log-)likelihood,
-
-$$
-\begin{aligned}
-\hat{\bm{w}} &= \argmax_{\bm{w}} \left(\log{ \mathbb{P}(\bm{y} | \bm{X}; \bm{w}) }\right) \\\
-&= \argmax_{\bm{w}} \sum_{t=1}^{T} {y_t} \log f(\bm{x}_t \cdot \bm{w}) -f(\bm{x}_t \cdot \bm{w}).
-\end{aligned}
-$$
-
-!!! note
-    - The logarithm is a monotonic function, therefore the log-likelihood and the likelihood are maximized by
-    the same weights $\hat{\bm{w}}$.
-    - The log-likelihood is numerically better behaved than the likelihood; A large product of small terms
-    (as in the likelihood) is likely to incur in numerical precision issues. This is why the log-likelihood is
-    maximized instead.
-    - In the second equation we omitted the $-\log(y_t!)$ term since it is constant in $\bm{w}$.
-    - For convex, monotonic functions, the log-likelihood is convex, i.e. the optimal weights exists and are
-    unique.
-
-## Learning the ML weights for the injected current example
-*Use the instantaneous injected current as a single scalar input; use nemos to learn the ML parameters (intercept and
-weight), check the model test-set fit accuracy.*
-
-
-## Adding Temporal Effects
-*Use the past history of the injected current as a predictor. Show that this is equivalent to
-convolve the weights with the current; the convolution can be represented as matrix vector product;
-show that for fine time resolution and long history it over-fits (too many params); introduce the basis as a way to
-reduce the parameter-space dimension and smoothen the history effect. show if the basis increase the
-fit accuracy*
+The start and end of each trial in seconds is stored in a pynapple IntervaSet object.
+Metadata about each trial (including the stimulation protocol used) will be stored in the `sweep_metadata` dictionary.
+Spike times will be stored as a pynapple Ts (time series) object.
 
 """
+import numpy as np
+import matplotlib.pyplot as plt
+from utils.load_allen_utils import load_to_pynapple
+
+# load the data
+(trial_interval_set,
+ injected_current,
+ response,
+ spike_times,
+ sweep_metadata) = load_to_pynapple("/Users/ebalzani/Code/fit_from_allensdk/cell_types/specimen_478498617/ephys.nwb")
+
+# print the metadata for a trial
+trial_id = 49
+print(f"Trial {trial_id} metadata")
+for key, values in sweep_metadata[trial_id].items():
+    print(f"\t - {key}: {values}")
+
+# plot the trial
+idx_interval = sweep_metadata[trial_id]["interval_set_index"]
+trial_interval = trial_interval_set[idx_interval: idx_interval+1]
+binned_current = injected_current.bin_average(bin_size=0.001, ep=trial_interval) * 10**12
+plt.figure()
+plt.title(f"Trial {trial_id}")
+plt.plot(binned_current, label="injected current")
+plt.vlines(spike_times[1].restrict(trial_interval).t, 0, 0.1 * np.max(binned_current), 'k', label="spikes")
+plt.legend()
+plt.xlabel('time [sec]', fontsize=10)
+plt.ylabel('current [pA]', fontsize=10)
+plt.tight_layout()
+
+# %%
+# We can plot the explore our dataset recording by plotting the time-course of the current injection
+# overlapped to the evoked spikes.
+
+# bin the injected current at 1ms resolution
+# and convert from Ampere to pico-Ampere
+binned_current = injected_current.bin_average(bin_size=0.001) * 10**12
+
+fig, ax = plt.subplots(figsize=(10, 3.5))
+plt.plot(binned_current, label="injected current")
+plt.vlines(spike_times[1].t, 0, 0.1 * np.max(binned_current), 'k', label="spikes")
+ylim = plt.ylim()
+for start, end in spike_times.time_support.values:
+    rect = plt.Rectangle(
+        xy=(start, 0),
+        width=end - start,
+        height=ylim[1],
+        alpha=0.2,
+        facecolor="grey",
+        label="trial" if start == 0 else None
+    )
+    ax.add_patch(rect)
+plt.xlabel('time [sec]', fontsize=10)
+plt.ylabel('current [pA]', fontsize=10)
+plt.legend()
+plt.tight_layout()
+
+# %%
+# *Work out minimal example of a poisson glm for predicting counts given an injected current.*
+#
+# # Learning The GLM Weights by Maximum Likelihood (ML)
+#
+# In general, we are provided with paired observations $(y_t, \bm{x}_t)$, for samples $t=1,...,T$,
+# where $y_t$ are the spike counts and $\bm{x}_t$ are some simultaneously recorded input features. If we evaluate
+# the encoding map (1) at the observations, we are left with a function of the weights, which is referred to as the
+# model likelihood. The ML estimator for the model weights is the set of weights that maximizes the (log-)likelihood,
+#
+# $$
+# \begin{aligned}
+# \hat{\bm{w}} &= \argmax_{\bm{w}} \left(\log{ \mathbb{P}(\bm{y} | \bm{X}; \bm{w}) }\right) \\\
+# &= \argmax_{\bm{w}} \sum_{t=1}^{T} {y_t} \log f(\bm{x}_t \cdot \bm{w}) -f(\bm{x}_t \cdot \bm{w}).
+# \end{aligned}
+# $$
+#
+# !!! note
+#     - The logarithm is a monotonic function, therefore the log-likelihood and the likelihood are maximized by
+#     the same weights $\hat{\bm{w}}$.
+#     - The log-likelihood is numerically better behaved than the likelihood; A large product of small terms
+#     (as in the likelihood) is likely to incur in numerical precision issues. This is why the log-likelihood is
+#     maximized instead.
+#     - In the second equation we omitted the $-\log(y_t!)$ term since it is constant in $\bm{w}$.
+#     - For convex, monotonic functions, the log-likelihood is convex, i.e. the optimal weights exists and are
+#     unique.
+#
+# ## Learning the ML weights for the injected current example
+# *Use the instantaneous injected current as a single scalar input; use nemos to learn the ML parameters (intercept and
+# weight), check the model test-set fit accuracy.*
+#
+#
+# ## Adding Temporal Effects
+# *Use the past history of the injected current as a predictor. Show that this is equivalent to
+# convolve the weights with the current; the convolution can be represented as matrix vector product;
+# show that for fine time resolution and long history it over-fits (too many params); introduce the basis as a way to
+# reduce the parameter-space dimension and smoothen the history effect. show if the basis increase the
+# fit accuracy*
+#
+# ## References
+#
+# 1. Weber, A. I., & Pillow, J. W. (2017). [Capturing the dynamical repertoire of single neurons with generalized linear
+#   models](https://pillowlab.princeton.edu/pubs/Weber17_IzhikevichGLM_NC.pdf). Neural computation, 29(12), 3260-3289.
+
